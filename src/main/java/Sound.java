18567e76e6a7716d28a5b3ac54f465a51b3f62bc
KellyTran2024 <ketran4@s.sfusd.edu>
2025-04-09 22:56:50 -0700

    Update Sound.java

--- src/main/java/Sound.java
+++ src/main/java/Sound.java
@@ -15,8 +15,17 @@ public class Sound
    */
   public int limitAmplitude(int limit)
   {  
-    /* to be implemented in part (a) */
-    return 0;
+  int count =0; 
+  for (int i = 0; i < samples.length; i++) {
+    if (samples[i] > limit) {
+      samples[i] = limit;
+      count++;
+    } else if (samples[i] < -limit) {
+      samples[i] = -limit;
+      count++;
+    }
+  }
+  return count;
   }
 
 
@@ -28,6 +37,12 @@ public class Sound
    */
   public void trimSilenceFromBeginning()
   {
-    /* to be implemented in part (b) */
+   int zeros =0; 
+    while(zeros < samples.length && samples[zeros] ==0){
+      zeros++;
+  }
+    int [] results = new int[samples.length - zeros];
+     for (int i = 0; i < results.length; i++) {
+    results[i] = samples[zeros + i];
   }
 }
